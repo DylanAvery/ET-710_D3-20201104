fcf66b0572933bf9bfbf880c67098cf2c18ad36e
DylanAvery <dylavery712@gmail.com>
2020-11-20 08:36:49 -0500

    section 2 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartService } from './cart.service';
+import { ShippingComponent } from './shipping/shipping.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -22,9 +25,12 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    ShippingComponent,
+    CartComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  providers: [CartService]
 })
 export class AppModule { }
 
