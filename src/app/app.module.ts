8cc0da623ac95a8ccb57f316e59a6ede65cab2c3
DylanAvery <dylavery712@gmail.com>
2020-11-20 08:44:47 -0500

    section 3 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -9,15 +10,24 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartService } from './cart.service';
-import { ShippingComponent } from './shipping/shipping.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { GraphicDesignComponent } from './graphic-design/graphic-design.component';
+import { IllustrationComponent } from './illustration/illustration.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
+      { path: 'graphic', component: GraphicDesignComponent },
+      { path: 'illustration', component: IllustrationComponent },
+
     ])
   ],
   declarations: [
@@ -26,8 +36,10 @@ import { CartComponent } from './cart/cart.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
+    CartComponent,
     ShippingComponent,
-    CartComponent
+    GraphicDesignComponent,
+    IllustrationComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
